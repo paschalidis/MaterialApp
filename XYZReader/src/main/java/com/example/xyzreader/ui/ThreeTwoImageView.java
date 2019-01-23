92b6828613f3b05124e1bb468efef2de9ce1f14e
Pavlos Paschalidis <ppaschalidis@customedialabs.com>
2019-01-23 16:33:49 +0200

    feat: detail view aspect radio images

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ThreeTwoImageView.java
@@ -0,0 +1,26 @@
+package com.example.xyzreader.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class ThreeTwoImageView extends android.support.v7.widget.AppCompatImageView {
+    public ThreeTwoImageView(Context context) {
+        super(context);
+    }
+
+    public ThreeTwoImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ThreeTwoImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int threeTwoHeight = MeasureSpec.getSize(widthMeasureSpec) * 2 / 3;
+        int threeTwoHeightSpec = MeasureSpec.makeMeasureSpec(threeTwoHeight, MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, threeTwoHeightSpec);
+    }
+}
