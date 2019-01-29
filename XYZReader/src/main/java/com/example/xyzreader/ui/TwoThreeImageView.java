3490e038043871be355c1c6c0a88f91c9f0392d2
Pavlos Paschalidis <ppaschalidis@customedialabs.com>
2019-01-29 16:11:12 +0200

    feat: mobile land design

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/TwoThreeImageView.java
@@ -0,0 +1,25 @@
+package com.example.xyzreader.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class TwoThreeImageView extends android.support.v7.widget.AppCompatImageView {
+    public TwoThreeImageView(Context context) {
+        super(context);
+    }
+
+    public TwoThreeImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TwoThreeImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int threeTwoHeight = MeasureSpec.getSize(widthMeasureSpec) * 3 / 2;
+        int threeTwoHeightSpec = MeasureSpec.makeMeasureSpec(threeTwoHeight, MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, threeTwoHeightSpec);
+    }
+}
